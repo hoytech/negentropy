540753c66ac7b4fcd3b255c9607e3f558a49cddd
Doug Hoyte <doug@hcsw.org>
2023-09-02 06:41:17 -0400

    test all lang combinations

--- test/test.pl
+++ test/test.pl
@@ -8,20 +8,22 @@ unlink("negent-test.log");
 my $langs = shift // 'cpp,js';
 
 
-foreach my $lang (split /,/, $langs) {
-    note("------LANG $lang ------");
+foreach my $lang1 (split /,/, $langs) {
+    foreach my $lang2 (split /,/, $langs) {
+        note("------LANG $lang1 / $lang2 ------");
 
-    note("Full upload");
-    run("RECS=100000 FRAMESIZELIMIT1=60000 FRAMESIZELIMIT2=500000 P1=1 P2=0 P3=0 perl fuzz.pl $lang $lang");
+        note("Full upload");
+        run("RECS=100000 FRAMESIZELIMIT1=60000 FRAMESIZELIMIT2=500000 P1=1 P2=0 P3=0 perl fuzz.pl $lang1 $lang2");
 
-    note("Full download");
-    run("RECS=100000 FRAMESIZELIMIT1=60000 FRAMESIZELIMIT2=500000 P1=0 P2=1 P3=0 perl fuzz.pl $lang $lang");
+        note("Full download");
+        run("RECS=100000 FRAMESIZELIMIT1=60000 FRAMESIZELIMIT2=500000 P1=0 P2=1 P3=0 perl fuzz.pl $lang1 $lang2");
 
-    note("Identical DBs");
-    run("RECS=100000 FRAMESIZELIMIT1=60000 FRAMESIZELIMIT2=500000 P1=0 P2=0 P3=1 perl fuzz.pl $lang $lang");
+        note("Identical DBs");
+        run("RECS=100000 FRAMESIZELIMIT1=60000 FRAMESIZELIMIT2=500000 P1=0 P2=0 P3=1 perl fuzz.pl $lang1 $lang2");
 
-    note("Mixed");
-    run("RECS=100000 FRAMESIZELIMIT1=60000 FRAMESIZELIMIT2=500000 P1=1 P2=1 P3=5 perl fuzz.pl $lang $lang");
+        note("Mixed");
+        run("RECS=100000 FRAMESIZELIMIT1=60000 FRAMESIZELIMIT2=500000 P1=1 P2=1 P3=5 perl fuzz.pl $lang1 $lang2");
+    }
 }
 
 
