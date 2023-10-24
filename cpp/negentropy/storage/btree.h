0c464f5eeea456db904755935b03de88081029f0
Doug Hoyte <doug@hcsw.org>
2023-10-24 17:35:49 -0400

    btree wip

--- cpp/negentropy/storage/btree.h
+++ cpp/negentropy/storage/btree.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <hoytech/hex.h> //FIXME
+
 #include "negentropy.h"
 
 
@@ -115,6 +117,7 @@ struct BTree /*: StorageBase*/ {
         if (!currNode.exists()) {
             LeafNode newLeaf;
 
+            newLeaf.items[0] = item;
             newLeaf.numItems++;
             newLeaf.accum.add(item.id);
 
@@ -123,7 +126,7 @@ struct BTree /*: StorageBase*/ {
         }
 
         while (!currNode.isLeaf()) {
-            throw err("not impl");
+            throw err("diving not impl");
         }
 
         if (currNode.leaf()->numItems < MAX_ITEMS_PER_LEAF_NODE) {
@@ -131,17 +134,40 @@ struct BTree /*: StorageBase*/ {
             LeafNode newLeaf = oldLeaf;
 
             size_t j = 0;
-            for (size_t i = 0; i < newLeaf.numItems; i++) {
-                if (oldLeaf.items[i] < item) {
-                    if (item == oldLeaf.items[i]) throw err("already exists");
+            for (size_t i = 0; i < oldLeaf.numItems; i++) {
+                if (item < oldLeaf.items[i]) {
                     newLeaf.items[j++] = item;
+                } else if (oldLeaf.items[i] == item) {
+                    throw err("already exists");
                 }
 
                 newLeaf.items[j++] = oldLeaf.items[i];
             }
 
+            if (j == oldLeaf.numItems) newLeaf.items[j++] = item;
+
             newLeaf.numItems++;
             newLeaf.accum.add(item.id);
+
+            saveNode(NodePtr{&newLeaf}, currNodeId);
+        } else {
+            // split
+            throw err("split not impl");
+        }
+    }
+
+    void walk(uint64_t nodeId = 1, int depth = 0) {
+        auto currNode = getNode(nodeId);
+        std::string indent(depth * 4, ' ');
+
+        if (currNode.isLeaf()) {
+            const auto &leaf = *currNode.leaf();
+
+            std::cout << indent << "LEAF id=" << nodeId << " numItems=" << leaf.numItems << " accum=" << hoytech::to_hex(leaf.accum.sv()) << std::endl;
+
+            for (size_t i = 0; i < leaf.numItems; i++) {
+                std::cout << indent << "  item: " << leaf.items[i].timestamp << "," << hoytech::to_hex(leaf.items[i].getId()) << std::endl;
+            }
         }
     }
 };
--- test/cpp/btree.cpp
+++ test/cpp/btree.cpp
@@ -14,15 +14,28 @@
 
 
 int main() {
-    std::cout << "SIZEOF LEAF: " << sizeof(negentropy::storage::LeafNode) << std::endl;
-    std::cout << "SIZEOF INTERIOR: " << sizeof(negentropy::storage::InteriorNode) << std::endl;
+    //std::cout << "SIZEOF LEAF: " << sizeof(negentropy::storage::LeafNode) << std::endl;
+    //std::cout << "SIZEOF INTERIOR: " << sizeof(negentropy::storage::InteriorNode) << std::endl;
 
     negentropy::storage::BTree btree;
 
+
     {
-        negentropy::Item item(100);
+        negentropy::Item item(100, std::string(32, '\x01'));
         btree.insert(item);
     }
+
+    {
+        negentropy::Item item(99, std::string(32, '\x01'));
+        btree.insert(item);
+    }
+
+    {
+        negentropy::Item item(101, std::string(32, '\x01'));
+        btree.insert(item);
+    }
+
+    btree.walk();
 
     return 0;
 }
