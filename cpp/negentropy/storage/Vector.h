08841a72c9619725c0fa06eab2bd62bbbd08ece7
Doug Hoyte <doug@hcsw.org>
2023-11-01 16:53:07 -0400

    cleanup

--- cpp/negentropy/storage/Vector.h
+++ cpp/negentropy/storage/Vector.h
@@ -14,8 +14,7 @@ struct Vector : StorageBase {
     void addItem(uint64_t createdAt, std::string_view id) {
         if (sealed) throw negentropy::err("already sealed");
         if (id.size() != ID_SIZE) throw negentropy::err("bad id size for added item");
-
-        items.emplace_back(createdAt, id.substr(0, ID_SIZE));
+        items.emplace_back(createdAt, id);
     }
 
     void seal() {
@@ -27,10 +26,6 @@ struct Vector : StorageBase {
         for (size_t i = 1; i < items.size(); i++) {
             if (items[i - 1] == items[i]) throw negentropy::err("duplicate item inserted");
         }
-    }
-
-    void checkSealed() {
-        if (!sealed) throw negentropy::err("not sealed");
     }
 
     uint64_t size() {
@@ -58,17 +53,20 @@ struct Vector : StorageBase {
     }
 
     Fingerprint fingerprint(size_t begin, size_t end) {
-        checkSealed();
-        if (begin > end || end > items.size()) throw negentropy::err("bad range");
-
         Accumulator out;
         out.setToZero();
 
-        for (size_t i = begin; i < end; ++i) {
-            out.add(items[i]);
-        }
+        iterate(begin, end, [&](const Item &item, size_t){
+            out.add(item);
+            return true;
+        });
 
         return out.getFingerprint(end - begin);
+    }
+
+  private:
+    void checkSealed() {
+        if (!sealed) throw negentropy::err("not sealed");
     }
 };
 
