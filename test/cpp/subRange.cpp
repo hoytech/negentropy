8b579de86f9c04b3f664a90c32ad14f1c9e3c81b
Doug Hoyte <doug@hcsw.org>
2023-12-06 14:00:39 -0500

    cpp subRange testing

--- test/cpp/subRange.cpp
+++ test/cpp/subRange.cpp
@@ -55,9 +55,63 @@ void testSubRange() {
 }
 
 
+
+template<typename T>
+void testSync() {
+    T vecBig;
+    T vecSmall;
+
+    for (size_t i = 0; i < 100'000; i++) {
+        if (i % 22'000 == 0) {
+            std::cout << "Setting extra: " << i << " : " << hoytech::to_hex(std::string(32, (unsigned char)(i % 256))) << std::endl;
+            continue;
+        }
+        vecBig.insert(100 + i, std::string(32, (unsigned char)(i % 256)));
+    }
+
+/*
+    for (size_t i = 20'000; i < 90'000; i++) {
+        if (i % 15'000 == 0) {
+            std::cout << "Setting missing: " << i << " : " << hoytech::to_hex(std::string(32, (unsigned char)(i % 256))) << std::endl;
+            continue;
+        }
+        vecSmall.insert(100 + i, std::string(32, (unsigned char)(i % 256)));
+    }
+    */
+
+    vecBig.seal();
+    vecSmall.seal();
+
+    negentropy::storage::SubRange subRange(vecBig, negentropy::Bound(100 + 20'000), negentropy::Bound(100 + 90'000));
+
+
+    auto ne1 = Negentropy(vecSmall, 20'000);
+    auto ne2 = Negentropy(subRange, 20'000);
+
+    std::string msg = ne1.initiate();
+
+    while (true) {
+        msg = ne2.reconcile(msg);
+
+        std::vector<std::string> have, need;
+        auto newMsg = ne1.reconcile(msg, have, need);
+
+        for (const auto &item : have) std::cout << "have," << hoytech::to_hex(item) << std::endl;
+        for (const auto &item : need) std::cout << "need," << hoytech::to_hex(item) << std::endl;
+
+        if (!newMsg) break;
+        else std::swap(msg, *newMsg);
+    }
+}
+
+
+
+
 int main() {
     testSubRange<negentropy::storage::Vector>();
     testSubRange<negentropy::storage::BTreeMem>();
+
+    testSync<negentropy::storage::BTreeMem>();
 
     std::cout << "OK" << std::endl;
 
