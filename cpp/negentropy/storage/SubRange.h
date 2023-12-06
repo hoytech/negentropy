3c14073add45341b3dbaeff4502f903e89365120
Doug Hoyte <doug@hcsw.org>
2023-12-06 14:42:08 -0500

    cpp subRange work

--- cpp/negentropy/storage/SubRange.h
+++ cpp/negentropy/storage/SubRange.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+
 #include "negentropy.h"
 
 
@@ -12,34 +14,37 @@ struct SubRange : StorageBase {
     size_t baseSize;
     size_t subBegin;
     size_t subEnd;
+    size_t subSize;
 
     SubRange(StorageBase &base, const Bound &lowerBound, const Bound &upperBound) : base(base) {
         baseSize = base.size();
         subBegin = lowerBound == Bound(0) ? 0 : base.findLowerBound(0, baseSize, lowerBound);
         subEnd = upperBound == Bound(MAX_U64) ? baseSize : base.findLowerBound(subBegin, baseSize, upperBound);
         if (subEnd != baseSize && Bound(base.getItem(subEnd)) == upperBound) subEnd++; // instead of upper_bound: OK because items are unique
+        subSize = subEnd - subBegin;
     }
 
     uint64_t size() {
-        return subEnd - subBegin;
+        return subSize;
     }
 
     const Item &getItem(size_t i) {
-        if (i >= baseSize) throw negentropy::err("bad index");
+        if (i >= subSize) throw negentropy::err("bad index");
         return base.getItem(subBegin + i);
     }
 
     void iterate(size_t begin, size_t end, std::function<bool(const Item &, size_t)> cb) {
         checkBounds(begin, end);
 
-        base.iterate(subBegin + begin, subBegin + end, cb);
+        base.iterate(subBegin + begin, subBegin + end, [&](const Item &item, size_t index){
+            return cb(item, index - subBegin);
+        });
     }
 
     size_t findLowerBound(size_t begin, size_t end, const Bound &bound) {
         checkBounds(begin, end);
 
-        auto ret = base.findLowerBound(subBegin + begin, subBegin + end, bound);
-        return ret - subBegin;
+        return std::min(base.findLowerBound(subBegin + begin, subBegin + end, bound) - subBegin, subSize);
     }
 
     Fingerprint fingerprint(size_t begin, size_t end) {
@@ -50,7 +55,7 @@ struct SubRange : StorageBase {
 
   private:
     void checkBounds(size_t begin, size_t end) {
-        if (begin > end || end > subEnd) throw negentropy::err("bad range");
+        if (begin > end || end > subSize) throw negentropy::err("bad range");
     }
 };
 
--- test/cpp/subRange.cpp
+++ test/cpp/subRange.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <set>
+
+#include <openssl/sha.h>
 
 #include <hoytech/error.h>
 #include <hoytech/hex.h>
@@ -10,17 +13,28 @@
 
 
 
+std::string sha256(std::string_view input) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256(reinterpret_cast<const unsigned char*>(input.data()), input.size(), hash);
+    return std::string((const char*)&hash[0], SHA256_DIGEST_LENGTH);
+}
+
+std::string uintToId(uint64_t id) {
+    return sha256(std::string((char*)&id, 8));
+}
+
+
 template<typename T>
 void testSubRange() {
     T vecBig;
     T vecSmall;
 
     for (size_t i = 0; i < 1000; i++) {
-        vecBig.insert(100 + i, std::string(32, (unsigned char)(i % 256)));
+        vecBig.insert(100 + i, uintToId(i));
     }
 
     for (size_t i = 400; i < 600; i++) {
-        vecSmall.insert(100 + i, std::string(32, (unsigned char)(i % 256)));
+        vecSmall.insert(100 + i, uintToId(i));
     }
 
     vecBig.seal();
@@ -57,32 +71,52 @@ void testSubRange() {
 
 
 template<typename T>
-void testSync() {
+void testSync(bool emptySide1, bool emptySide2) {
     T vecBig;
     T vecSmall;
 
-    for (size_t i = 0; i < 100'000; i++) {
-        if (i % 22'000 == 0) {
-            std::cout << "Setting extra: " << i << " : " << hoytech::to_hex(std::string(32, (unsigned char)(i % 256))) << std::endl;
+    std::set<std::string> expectedHave;
+    std::set<std::string> expectedNeed;
+
+    size_t const lowerLimit = 20'000;
+    size_t const upperLimit = 90'000;
+
+    for (size_t i = lowerLimit; i < upperLimit; i++) {
+        auto id = uintToId(i);
+        if (emptySide1 || i % 15'000 == 0) {
+            if (i >= lowerLimit && i < upperLimit) expectedNeed.insert(id);
             continue;
         }
-        vecBig.insert(100 + i, std::string(32, (unsigned char)(i % 256)));
+        vecSmall.insert(100 + i, id);
     }
 
-/*
-    for (size_t i = 20'000; i < 90'000; i++) {
-        if (i % 15'000 == 0) {
-            std::cout << "Setting missing: " << i << " : " << hoytech::to_hex(std::string(32, (unsigned char)(i % 256))) << std::endl;
+    for (size_t i = 0; i < 100'000; i++) {
+        auto id = uintToId(i);
+        if (emptySide2 || i % 22'000 == 0) {
+            if (i >= lowerLimit && i < upperLimit) expectedHave.insert(id);
             continue;
         }
-        vecSmall.insert(100 + i, std::string(32, (unsigned char)(i % 256)));
+        vecBig.insert(100 + i, id);
     }
-    */
+
+    // Get rid of common
+
+    std::set<std::string> commonItems;
+
+    for (const auto &item : expectedHave) {
+        if (expectedNeed.contains(item)) commonItems.insert(item);
+    }
+
+    for (const auto &item : commonItems) {
+        expectedHave.erase(item);
+        expectedNeed.erase(item);
+    }
+
 
     vecBig.seal();
     vecSmall.seal();
 
-    negentropy::storage::SubRange subRange(vecBig, negentropy::Bound(100 + 20'000), negentropy::Bound(100 + 90'000));
+    negentropy::storage::SubRange subRange(vecBig, negentropy::Bound(100 + lowerLimit), negentropy::Bound(100 + upperLimit));
 
 
     auto ne1 = Negentropy(vecSmall, 20'000);
@@ -96,12 +130,22 @@ void testSync() {
         std::vector<std::string> have, need;
         auto newMsg = ne1.reconcile(msg, have, need);
 
-        for (const auto &item : have) std::cout << "have," << hoytech::to_hex(item) << std::endl;
-        for (const auto &item : need) std::cout << "need," << hoytech::to_hex(item) << std::endl;
+        for (const auto &item : have) {
+            if (!expectedHave.contains(item)) throw hoytech::error("unexpected have: ", hoytech::to_hex(item));
+            expectedHave.erase(item);
+        }
+
+        for (const auto &item : need) {
+            if (!expectedNeed.contains(item)) throw hoytech::error("unexpected need: ", hoytech::to_hex(item));
+            expectedNeed.erase(item);
+        }
 
         if (!newMsg) break;
         else std::swap(msg, *newMsg);
     }
+
+    if (expectedHave.size()) throw hoytech::error("missed have");
+    if (expectedNeed.size()) throw hoytech::error("missed need");
 }
 
 
@@ -111,7 +155,9 @@ int main() {
     testSubRange<negentropy::storage::Vector>();
     testSubRange<negentropy::storage::BTreeMem>();
 
-    testSync<negentropy::storage::BTreeMem>();
+    testSync<negentropy::storage::Vector>(false, false);
+    testSync<negentropy::storage::Vector>(true, false);
+    testSync<negentropy::storage::Vector>(false, true);
 
     std::cout << "OK" << std::endl;
 
